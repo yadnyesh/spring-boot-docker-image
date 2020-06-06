0f3c5d60ff67b924fb54fdab9ad66743b4905b3b
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-06 12:24:12 +0530

    First Commit

---
+++ src/main/java/yb/yadnyesh/springbootdockerimage/SpringBootDockerImageApplication.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.springbootdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yb/yadnyesh/springbootdockerimage/SpringBootDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.springbootdockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
